62601ebc4102029a0a2966c54837f1b07c48bdf0
kamilo116 <kamil.sladowski@gmail.com>
2018-07-22 17:33:24 +0200

    Create arduino_grpc.py

---
+++ arduino_grpc.py
@@ -0,0 +1,21 @@
+import serial
+from time import sleep
+
+arduinoData = serial.Serial('com3', 9600)
+
+def ledOn():
+	arduinoData.write(b'1')
+	
+def ledOff():
+	arduinoData.write(b'0')
+	
+i = 0
+print("start")
+while i < 10:
+	print(i)
+	ledOn()
+	sleep(1)
+	ledOff()
+	sleep(1)
+	i +=1
+print("end")
