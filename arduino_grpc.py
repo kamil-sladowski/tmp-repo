2e40322291b572866640dff44f8e917635ce7a58
kamilo116 <kamil.sladowski@gmail.com>
2018-07-25 20:53:36 +0200

    Update arduino_grpc.py

--- arduino_grpc.py
+++ arduino_grpc.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
 
-arduinoData = serial.Serial('/dev/tty', 9600, timeout=5)
+arduinoData = serial.Serial('COM3', 9600, timeout=5)
 
 def ledOn():
 	arduinoData.write(b'1')
